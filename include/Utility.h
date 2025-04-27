cd6bf7a500caeb84eb039bf35021163b58d4e8a7
LauraKoraeva <laurakoraeva@gmail.com>
2025-04-28 00:19:37 +0300

    Utility added

---
+++ include/Utility.h
@@ -0,0 +1,3 @@
+#pragma once
+
+bool correctInput(int input);
---
+++ src/Utility.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include "Utility.h"
+
+bool correctInput(int input)
+{
+    if (std::cin.fail() || std::cin.peek() != '\n')
+    {
+        std::cerr << "Incorrect input.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return false;
+    }
+    else
+        return true;
+}
